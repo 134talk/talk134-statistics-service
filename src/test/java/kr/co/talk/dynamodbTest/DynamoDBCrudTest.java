22e934614bcd5759f006298fc3ad9bc6ee09af23
Min Hong <shmin7777@naver.com>
2023-06-23 18:45:05 +0900

    test code 짜는중

--- src/test/java/kr/co/talk/dynamodbTest/DynamoDBCrudTest.java
+++ src/test/java/kr/co/talk/dynamodbTest/DynamoDBCrudTest.java
@@ -2,9 +2,11 @@ package kr.co.talk.dynamodbTest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,127 +21,140 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.talk.domain.statistics.dto.EmoticonCode;
 import kr.co.talk.domain.statistics.model.StatisticsEntity;
+import kr.co.talk.domain.statistics.model.StatisticsEntity.KeywordSet;
+import kr.co.talk.domain.statistics.model.StatisticsEntity.RoomEmoticon;
 import kr.co.talk.domain.statistics.model.StatisticsEntity.Users;
+import kr.co.talk.domain.statistics.repository.StatisticsRepository;
 
 @SpringBootTest
 public class DynamoDBCrudTest {
 
-	@Autowired
-	private DynamoDBMapper dynamoDBMapper;
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
 
-	@Autowired
-	private AmazonDynamoDB amazonDynamoDB;
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
 
-	@Autowired
-	ObjectMapper mapper;
+    @Autowired
+    private StatisticsRepository statisticsRepository;
 
-	@BeforeEach
-	public void setup() throws Exception {
-		dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+    @Autowired
+    ObjectMapper mapper;
 
-		CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(StatisticsEntity.class);
-		ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput(5L, 5L);
-		tableRequest.setProvisionedThroughput(provisionedThroughput);
-		tableRequest.getGlobalSecondaryIndexes().forEach(v -> v.setProvisionedThroughput(provisionedThroughput));
+    @BeforeEach
+    public void setup() throws Exception {
+        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
 
-		boolean created = TableUtils.createTableIfNotExists(amazonDynamoDB, tableRequest);
+        CreateTableRequest tableRequest =
+                dynamoDBMapper.generateCreateTableRequest(StatisticsEntity.class);
+        ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput(5L, 5L);
+        tableRequest.setProvisionedThroughput(provisionedThroughput);
+        tableRequest.getGlobalSecondaryIndexes()
+                .forEach(v -> v.setProvisionedThroughput(provisionedThroughput));
 
-		if (created) {
-			System.out.println("테이블 생성됨");
-		} else {
-			System.out.println("이미 테이블 있음");
-		}
-	}
+        boolean created = TableUtils.createTableIfNotExists(amazonDynamoDB, tableRequest);
 
-	@Test
-	@DisplayName("statistics save test")
-	public void saveTest() {
-		List<Users> users = new ArrayList<>();
+        if (created) {
+            System.out.println("테이블 생성됨");
+        } else {
+            System.out.println("이미 테이블 있음");
+        }
+    }
 
-		users.add(Users.builder().userId(82).score(5).sentence("test sentence").build());
+    @Test
+    @DisplayName("statistics save test")
+    public void saveTest() {
+        List<Users> users = new ArrayList<>();
 
-		users.add(Users.builder().userId(78).score(8).sentence("test sentence2").build());
+        users.add(Users.builder().userId(82).score(5).sentence("test sentence").build());
 
-		StatisticsEntity entity = StatisticsEntity.builder().roomId(48).teamCode("uaCGJl").chatroomEndtime(LocalDateTime.now())
-				.users(users).build();
+        users.add(Users.builder().userId(78).score(8).sentence("test sentence2").build());
 
-		dynamoDBMapper.save(entity);
-	}
+        StatisticsEntity entity = StatisticsEntity.builder().roomId(48).teamCode("uaCGJl")
+                .chatroomEndtime(LocalDateTime.now())
+                .users(users).build();
 
-	@Test
-	@DisplayName("statistics find test")
-	public void findTest() {
-		StatisticsEntity load = dynamoDBMapper.load(StatisticsEntity.class, 48);
-		System.out.println("load::" + load);
-	}
+        dynamoDBMapper.save(entity);
+    }
 
-	@Test
-	@DisplayName("팀 코드만 가지고 chatroom 전체 조회")
-	public void findAllWithTeamCode() {
-		Map<String, AttributeValue> params = new HashMap<>();
-		params.put(":v1", new AttributeValue().withS("teamCode"));
-		
+    @Test
+    @DisplayName("statistics find test")
+    public void findTest() {
+        StatisticsEntity load = dynamoDBMapper.load(StatisticsEntity.class, 48);
+        System.out.println("load::" + load);
+    }
 
-		DynamoDBQueryExpression<StatisticsEntity> dbQueryExpression = new DynamoDBQueryExpression<StatisticsEntity>()
-				.withConsistentRead(false)
-				.withIndexName("team-code-idx")
-				.withKeyConditionExpression("teamCode = :v1")
-				.withExpressionAttributeValues(params);
-		
-//		DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
-//		
-//		Table table = dynamoDB.getTable("STATISTICS");
-//		
-//		Index teamCodeIdx = table.getIndex("team-code-idx");
-//		ItemCollection<QueryOutcome> items = teamCodeIdx.query(new QuerySpec()
-//		        .withKeyConditionExpression("teamCode = :v1")
-//		        .withValueMap(new ValueMap().withString(":v1", "uaCGJl"))
-		
-//		
-//		Iterator<Item> iter = items.iterator();
-//		
-//		while(iter.hasNext()) {
-//		    Item next = iter.next();
-//		    System.out.println(next.toJSONPretty());
-//		}
-		
-		
-		List<StatisticsEntity> query = dynamoDBMapper.query(StatisticsEntity.class, dbQueryExpression);
-
-		// query를 보게 되면 pk값만 가져오게 된다
-		System.out.println("query:::" + query);
+    @Test
+    @DisplayName("팀 코드만 가지고 chatroom 전체 조회")
+    public void findAllWithTeamCode() {
+        Map<String, AttributeValue> params = new HashMap<>();
+        params.put(":v1", new AttributeValue().withS("teamCode"));
 
 
-		
-		
-	}
+        DynamoDBQueryExpression<StatisticsEntity> dbQueryExpression =
+                new DynamoDBQueryExpression<StatisticsEntity>()
+                        .withConsistentRead(false)
+                        .withIndexName("team-code-idx")
+                        .withKeyConditionExpression("teamCode = :v1")
+                        .withExpressionAttributeValues(params);
 
-	// scan - full scan
-	// query - key 를 기반으로 scan
-	@Test
-	@DisplayName("statistics find with query")
-	public void queryTest() {
-		Map<String, AttributeValue> params = new HashMap<>();
-		params.put("roomId", new AttributeValue().withS("48"));
+        // DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+        //
+        // Table table = dynamoDB.getTable("STATISTICS");
+        //
+        // Index teamCodeIdx = table.getIndex("team-code-idx");
+        // ItemCollection<QueryOutcome> items = teamCodeIdx.query(new QuerySpec()
+        // .withKeyConditionExpression("teamCode = :v1")
+        // .withValueMap(new ValueMap().withString(":v1", "uaCGJl"))
 
-		StatisticsEntity statisticsEntity = new StatisticsEntity();
-		statisticsEntity.setRoomId(48);
+        //
+        // Iterator<Item> iter = items.iterator();
+        //
+        // while(iter.hasNext()) {
+        // Item next = iter.next();
+        // System.out.println(next.toJSONPretty());
+        // }
 
-		DynamoDBQueryExpression<StatisticsEntity> dbQueryExpression = new DynamoDBQueryExpression<StatisticsEntity>()
-				.withHashKeyValues(statisticsEntity);
 
-		List<StatisticsEntity> query = dynamoDBMapper.query(StatisticsEntity.class, dbQueryExpression);
+        List<StatisticsEntity> query =
+                dynamoDBMapper.query(StatisticsEntity.class, dbQueryExpression);
 
-		System.out.println(query);
-	}
-	
-	@Test
-	@DisplayName("리포트 상세 조회")
-	public void reportDetailTest() {
+        // query를 보게 되면 pk값만 가져오게 된다
+        System.out.println("query:::" + query);
+
+
+
+    }
+
+    // scan - full scan
+    // query - key 를 기반으로 scan
+    @Test
+    @DisplayName("statistics find with query")
+    public void queryTest() {
+        Map<String, AttributeValue> params = new HashMap<>();
+        params.put("roomId", new AttributeValue().withS("48"));
+
+        StatisticsEntity statisticsEntity = new StatisticsEntity();
+        statisticsEntity.setRoomId(48);
+
+        DynamoDBQueryExpression<StatisticsEntity> dbQueryExpression =
+                new DynamoDBQueryExpression<StatisticsEntity>()
+                        .withHashKeyValues(statisticsEntity);
+
+        List<StatisticsEntity> query =
+                dynamoDBMapper.query(StatisticsEntity.class, dbQueryExpression);
+
+        System.out.println(query);
+    }
+
+    @Test
+    @DisplayName("리포트 상세 조회")
+    public void reportDetailTest() {
         List<StatisticsEntity> statisticsEntityList = new ArrayList<>();
         String teamCode = "uaCGJl";
-        
+
         Map<String, AttributeValue> params = new HashMap<>();
         params.put(":v1", new AttributeValue().withS(teamCode));
 
@@ -159,10 +174,58 @@ public class DynamoDBCrudTest {
 
         roomIds.forEach(
                 rid -> statisticsEntityList.add(dynamoDBMapper.load(StatisticsEntity.class, rid)));
-        
-        System.out.println("statisticsEntityList:::"+statisticsEntityList);
-        
-	}
+
+        System.out.println("statisticsEntityList:::" + statisticsEntityList);
+
+    }
 
 
+    @Test
+    @DisplayName("대화기록 리포트 상세 조회")
+    void chatLogDetailTest() {
+        String teamCode = "uaCGJl";
+        List<KeywordSet> keywordSet = new ArrayList<>();
+        List<RoomEmoticon> roomEmoticon = new ArrayList<>();
+
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP1).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP1).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP2).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP2).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP4).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP5).build());
+        roomEmoticon.add(RoomEmoticon.builder().emoticonCode(EmoticonCode.EMOTICON_TP1).build());
+
+        List<StatisticsEntity> statisticsListByTeamCode =
+                statisticsRepository.getStatisticsListByTeamCode(teamCode);
+
+        statisticsListByTeamCode.forEach(se -> {
+            keywordSet.add(se.getKeywordSet());
+            roomEmoticon.addAll(se.getRoomEmoticons());
+        });
+
+        Map<EmoticonCode, List<RoomEmoticon>> collect = roomEmoticon.stream()
+                .collect(Collectors.groupingBy(RoomEmoticon::getEmoticonCode));
+
+        
+//        Map<EmoticonCode, Long> collect2 = roomEmoticon.stream()
+//                .collect(
+//                        Collectors.groupingBy(RoomEmoticon::getEmoticonCode, Collectors.counting()))
+//                .entrySet().stream()
+//                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+//                .collect(Collectors.toMap(Entry::getKey, entry-> entry.getValue()))
+//                .limit(3)
+//                .collect(Collectors.toMap(Entry::getKey, entry-> entry.getValue() / roomEmoticon.size()));
+        
+    Map<EmoticonCode, Long> collect2 = roomEmoticon.stream()
+      .collect(
+              Collectors.groupingBy(RoomEmoticon::getEmoticonCode, Collectors.counting()));
+        
+    Map<EmoticonCode, Long> collect3 = collect2.entrySet().stream()
+      .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+      .limit(3)
+      .collect(Collectors.toMap(Entry::getKey, entry-> entry.getValue()));
+        
+        System.out.println("collect2:::"+collect2);
+
+    }
 }
